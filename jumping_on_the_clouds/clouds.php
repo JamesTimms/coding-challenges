37ba57534426cf5b3afe5f0dc48eea35535eaa2a
James Timms <james@docker-dev-en2>
2020-03-06 15:04:49 +0000

    Adding solution for Jump on the Clouds.

--- jumping_on_the_clouds/clouds.php
+++ jumping_on_the_clouds/clouds.php
@@ -1,22 +1,38 @@
 <?php
 
+const CUMULUS = 0;
+const THUNDERHEAD = 1;
+
 // Complete the jumpingOnClouds function below.
-function jumpingOnClouds($c) {
-
-
+function jumpingOnClouds($clouds) {
+  $cur_pos = 0;
+  $steps = 0;
+  for($pos = 0; $pos < (count($clouds)-1);) {
+    if(($pos + 2) < count($clouds) && $clouds[$pos + 2] == CUMULUS) {
+      $pos += 2;
+      $steps++;
+      continue;
+    }
+    elseif($clouds[$pos + 1] == CUMULUS) {
+      $pos++;
+      $steps++;
+      continue;
+    }
+  }
+  return $steps;
 }
 
 $stdout = fopen("php://stdout", "w");
 
 $stdin = fopen("php://stdin", "r");
 
-fscanf($stdin, "%d\n", $n);
+fscanf($stdin, "%d\n", $cloud_count);
 
-fscanf($stdin, "%[^\n]", $c_temp);
+fscanf($stdin, "%[^\n]", $clouds_raw);
 
-$c = array_map('intval', preg_split('/ /', $c_temp, -1, PREG_SPLIT_NO_EMPTY));
+$clouds = array_map('intval', preg_split('/ /', $clouds_raw, -1, PREG_SPLIT_NO_EMPTY));
 
-$result = jumpingOnClouds($c);
+$result = jumpingOnClouds($clouds);
 
 fwrite($stdout, $result . "\n");
 
