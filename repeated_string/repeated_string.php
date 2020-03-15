500bec78860e638adb407d6d1d2db59035ecc398
James Timms <james@reebric.com>
2020-03-15 19:17:09 +0000

    Added an optimised solution for larger repeating strings.

--- repeated_string/repeated_string.php
+++ repeated_string/repeated_string.php
@@ -5,7 +5,6 @@ const TARGET_CHAR = 'a';
 // A quick way of getting the char count for the caclulated string but not very efficient.
 function naiveRepeatedString($str_sequence, $sampling_size) {
   $sequence_len = strlen($str_sequence);
-
   $repeat_for = floor($sampling_size/$sequence_len);
   $remainder = $sampling_size % $sequence_len;
 
@@ -13,6 +12,20 @@ function naiveRepeatedString($str_sequence, $sampling_size) {
 
   $char_count = count_chars($calcd_string, 0)[ord(TARGET_CHAR)];
   
+  return $char_count;
+}
+
+function efficientRepeatedString($str_sequence, $sampling_size) {
+  $sequence_len = strlen($str_sequence);
+  $seq_mult = floor($sampling_size/$sequence_len);
+  $remainder = $sampling_size % $sequence_len;
+
+  $str_padding = str_pad('', $remainder, $str_sequence);
+  $padding_char_count = count_chars($str_padding, 0)[ord(TARGET_CHAR)];
+  
+  $seq_char_count = count_chars($str_sequence, 0)[ord(TARGET_CHAR)];
+  
+  $char_count = ($seq_char_count * $seq_mult) + $padding_char_count;
   return $char_count;
 }
 
@@ -26,7 +39,7 @@ fscanf($stdin, "%[^\n]", $str_sequence);
 fscanf($stdin, "%ld\n", $sampling_size);
 
 $result = 0;
-$result = naiveRepeatedString($str_sequence, $sampling_size);
+$result = efficientRepeatedString($str_sequence, $sampling_size);
 
 fwrite($stout, "result: " . $result . "\n");
 
