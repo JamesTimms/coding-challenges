bfe3e71f416582d06b5a8546eee99752ef22192a
James Timms <james@reebric.com>
2020-04-11 12:33:55 +0000

    Added a seperate function for when ratio is 1. Still need some work to beat all edge cases and large arrays.

--- count_triplets/triplets_counter.php
+++ count_triplets/triplets_counter.php
@@ -1,9 +1,12 @@
 <?php
 
 function count_triplets($indicies, $ratio) {
+  if($ratio==1) {
+    return ratio_of_one(count($indicies));
+  }
   $count = 0;
-  $indicies_dict = arr_to_dict_counter($indicies);
-  foreach($indicies_dict as $index => $num) {    
+  $indicies_dict = arr_to_dict_tracker($indicies);
+  foreach($indicies_dict as $index => $pos_arr) {    
     if(has_triplet($indicies_dict, $index, $ratio)) {  
       $count+=naive_triplet_parings($indicies_dict, $index, $ratio);
     }
@@ -32,8 +35,34 @@ function naive_triplet_parings($dict, $index, $ratio) {
   return $parings;
 }
 
+function wip_triplet_parings($dict, $index, $ratio) {
+  $parings = 0;
+  $t1 = $index;
+  $t2 = $t1 * $ratio;
+  $t3 = $t2 * $ratio;
+  foreach($dict[$t1] as $i) {
+    foreach($dict[$t2] as $j) {
+      foreach($dict[$t3] as $k) {
+        if($i < $j && $j < $k) $parings++;
+      }
+    }
+  }
+  return $parings;
+}
+
+function ratio_of_one($n) {
+  $count = 0;
+  $max = $n-1;
+  for($i=1; $i <= ($max-1); $i++) {
+    for($j=0; $j <= ($max-$i); $j++) {
+      $count+=$j;
+    }
+  } 
+  return $count;
+}
+
 # Change array to dictionary but track repeating elements.
-function arr_to_dict_counter($arr) {
+function arr_to_dict_tracker($arr) {
   $dict = array();
   foreach($arr as $pos => $value) {
     # Add to or intialise tracker, where the tracker is an array of positions stored in a dict of values.
