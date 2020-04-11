5de8b0c83cd499437980a88f19a1dcfc273e4154
James Timms <james@reebric.com>
2020-04-11 11:42:56 +0000

    Created a naive solution for counting parings.

--- count_triplets/triplets_counter.php
+++ count_triplets/triplets_counter.php
@@ -5,28 +5,40 @@ function count_triplets($indicies, $ratio) {
   $indicies_dict = arr_to_dict_counter($indicies);
   foreach($indicies_dict as $index => $num) {    
     if(has_triplet($indicies_dict, $index, $ratio)) {  
-      $count+=triplet_parings($indicies_dict, $index, $ratio);
+      $count+=naive_triplet_parings($indicies_dict, $index, $ratio);
     }
   }
   return $count;
 }
 
-function has_triplet($dict, $index, $ratio){
+function has_triplet($dict, $index, $ratio) {
   return array_key_exists($index, $dict) && 
         array_key_exists($index*$ratio, $dict) && 
         array_key_exists($index*$ratio*$ratio, $dict);
 }
 
-# Assumes there is at least one triplet pairing
-function triplet_parings($dict, $index, $ratio) {
-  return $dict[$index] * $dict[$index*$ratio] * $dict[$index*$ratio*$ratio];
+function naive_triplet_parings($dict, $index, $ratio) {
+  $parings = 0;
+  $t1 = $index;
+  $t2 = $t1 * $ratio;
+  $t3 = $t2 * $ratio;
+  foreach($dict[$t1] as $i) {
+    foreach($dict[$t2] as $j) {
+      foreach($dict[$t3] as $k) {
+        if($i < $j && $j < $k) $parings++;
+      }
+    }
+  }
+  return $parings;
 }
 
-# Change array to dictionary but counter number of repeating elements.
+# Change array to dictionary but track repeating elements.
 function arr_to_dict_counter($arr) {
   $dict = array();
-  foreach($arr as $value) {
-    isset($dict[$value]) ? $dict[$value]++ : $dict[$value] = 1;
+  foreach($arr as $pos => $value) {
+    # Add to or intialise tracker, where the tracker is an array of positions stored in a dict of values.
+    # Positions are stored in ascending order.
+    isset($dict[$value]) ? $dict[$value][] = $pos : $dict[$value] = [$pos];
   }
   return $dict;
 }
